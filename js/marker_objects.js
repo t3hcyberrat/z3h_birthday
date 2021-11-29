c2e03ed418982467f3360761bc7b402d75d0c9de
T3hCyberRat <t3hcyberrat@gmail.com>
2021-11-29 13:48:24 +0200

    Check

--- js/marker_objects.js
+++ js/marker_objects.js
@@ -1,13 +1,51 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.Camera();
+scene.add(Camera);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let ArToolkitSource = THREEx.ArToolkitSource({
+    // type of source - ['webcam', 'image', 'video']
+    sourceType: "webcam",
+});
+
+ArToolkitSource.init(() => {
+    setTimeout(() => {
+        ArToolkitSource.onResizeElement();
+        ArToolkitSource.copyElementSizeTo(renderer.domElement);
+    }, 2000);
+});
+
+let ArToolkitContext = new THREEx.ArToolkitContext({
+    cameraParametersUrl: '../camera_para.dat',
+    detectionMode: 'color_and_matrix'
+});
+
+ArToolkitContext.init(() => {
+    camera.ProjectionMatrix.copy(ArToolkitContext.getProjectionMatrix());
+});
+
+let ArMarkerControls = new THREEx.ArMarkerControls(ArToolkitContext, camera, {
+    type: 'pattern',
+    patternUrl: '../markers/1/',
+    changeMatrixMode: 'cameraTransformMatrix'
+});
+
+scene.visible = false;
+
+const geometry = new THREE.CubeGeometry(1, 1, 1);
+const material = new THREE.MeshNormalMaterial({ 
+    trasnparent: true,
+    opacity: 0.5,
+    side: THREE.DoubleSide
+ });
 const cube = new THREE.Mesh(geometry, material);
+cube.position.y = geometry.parameters.height / 2;
 scene.add(cube);
 
 camera.position.z = 5;
@@ -15,8 +53,8 @@ camera.position.z = 5;
 const animate = function () {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    ArToolkitContext.update(ArToolkitSource.domElement);
+    scene.visible = camera.visible;
 
     renderer.render(scene, camera);
 };
