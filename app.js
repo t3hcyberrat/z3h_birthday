acd800a82f046aa42980de4ded818f57cd29b3d1
T3hCyberRat <t3hcyberrat@gmail.com>
2021-11-29 14:01:38 +0200

    Moved app.js to ignored

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-const PORT = process.env.PORT || 3001;
-
-app.use('/', express.static(path.join(__dirname, '/')));
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
